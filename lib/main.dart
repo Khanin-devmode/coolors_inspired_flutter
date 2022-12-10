86e1a03e0da413165d3f9da69128ce3af09e1bef
Khanin <khanin.dev@gmail.com>
2022-12-10 19:39:50 +0700

    Add lock property and simply widget inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:random_color/random_color.dart';
 import 'package:shake/shake.dart';
@@ -33,11 +35,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<ColorObj> ColorObjList = [
-    ColorObj(Colors.red, 'Color 1 Hex Code'),
-    ColorObj(Colors.green, 'Color 2 Hex Code'),
-    ColorObj(Colors.blue, 'Color 3 Hex Code'),
-    ColorObj(Colors.yellow, 'Color 4 Hex Code'),
-    ColorObj(Colors.purple, 'Color 5 Hex Code'),
+    ColorObj(Colors.red, 'Color 1 Hex Code', false),
+    ColorObj(Colors.green, 'Color 2 Hex Code', false),
+    ColorObj(Colors.blue, 'Color 3 Hex Code', false),
+    ColorObj(Colors.yellow, 'Color 4 Hex Code', false),
+    ColorObj(Colors.purple, 'Color 5 Hex Code', false),
   ];
 
   void _generateColor() {
@@ -72,9 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
       child: Scaffold(
           body: Column(
-            children: ColorObjList.map(
-                    (e) => new ColorRow(color: e.color, colorCode: e.colorCode))
-                .toList(),
+            children: ColorObjList.map((e) => ColorRow(colorObj: e)).toList(),
           ),
           bottomNavigationBar: Container(
             height: 80,
@@ -87,23 +87,24 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class ColorRow extends StatelessWidget {
-  ColorRow({Key? key, required this.color, required this.colorCode})
-      : super(key: key);
+  ColorRow({
+    Key? key,
+    required this.colorObj,
+  }) : super(key: key);
 
-  Color color;
-  String colorCode;
+  ColorObj colorObj;
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
       child: Container(
         constraints: const BoxConstraints.expand(),
-        color: color,
+        color: colorObj.color,
         child:
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text(colorCode),
+            child: Text(colorObj.colorCode),
           ),
           const Padding(
             padding: EdgeInsets.all(8.0),
@@ -116,8 +117,9 @@ class ColorRow extends StatelessWidget {
 }
 
 class ColorObj {
-  ColorObj(this.color, this.colorCode);
+  ColorObj(this.color, this.colorCode, this.isLocked);
 
   Color color;
   String colorCode;
+  bool isLocked;
 }
