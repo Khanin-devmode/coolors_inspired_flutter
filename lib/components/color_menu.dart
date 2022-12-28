2c3fa7cbabf4a81a6552c9ac89a5fe5d3bf7607b
Khanin <khanin.dev@gmail.com>
2022-12-28 20:50:25 +0700

    Menu item ui

--- lib/components/color_menu.dart
+++ lib/components/color_menu.dart
@@ -8,44 +8,98 @@ Future<dynamic> showColorMenu(BuildContext context) {
           decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.vertical(top: Radius.circular(12))),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: const <Widget>[
-                // const Text('Modal BottomSheet'),
-                // ElevatedButton(
-                //   child: const Text('Close BottomSheet'),
-                //   onPressed: () => Navigator.pop(context),
-                // ),
-                MenuItem(
-                  iconData: Icons.favorite,
-                  label: 'Favorite',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // const Text('Modal BottomSheet'),
+              // ElevatedButton(
+              //   child: const Text('Close BottomSheet'),
+              //   onPressed: () => Navigator.pop(context),
+              // ),
+              MenuItem(
+                iconData: Icons.colorize,
+                label: 'Pick Colors',
+                hasNavigation: true,
+              ),
+              Divider(),
+              MenuItem(
+                iconData: Icons.add,
+                label: 'Add Colors',
+                hasNavigation: false,
+              ),
+              Divider(),
+              MenuItem(
+                iconData: Icons.remove,
+                label: 'Remove Color',
+                hasNavigation: false,
+              ),
+              Divider(),
+              MenuItem(
+                iconData: Icons.save,
+                label: 'Save Color',
+                hasNavigation: false,
+              ),
+              Divider(),
+              MenuItem(
+                iconData: Icons.copy,
+                label: 'Copy Color',
+                hasNavigation: false,
+              ),
+              Divider(),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text('Cancel')],
                 ),
-                Divider(),
-                MenuItem(iconData: Icons.delete, label: 'delete'),
-                Divider(),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
           ),
         );
       });
 }
 
 class MenuItem extends StatelessWidget {
-  const MenuItem({Key? key, required this.iconData, required this.label})
+  const MenuItem(
+      {Key? key,
+      required this.iconData,
+      required this.label,
+      required this.hasNavigation})
       : super(key: key);
 
   final IconData iconData;
   final String label;
+  final bool hasNavigation;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Icon(iconData), Text(label)],
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Icon(iconData),
+                SizedBox(width: 16),
+                Text(label),
+              ],
+            ),
+          ),
+          Container(
+            child: hasNavigation
+                ? Icon(
+                    Icons.arrow_forward,
+                    color: Colors.grey,
+                  )
+                : null,
+          ),
+        ],
       ),
     );
   }
