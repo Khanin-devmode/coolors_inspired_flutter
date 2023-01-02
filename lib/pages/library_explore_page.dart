8ffd8344705b1c6e3997e6e27512fbfa8680f3ec
Khanin <khanin.dev@gmail.com>
2023-01-02 20:35:51 +0700

    move tab menu to app bar

--- lib/pages/library_explore_page.dart
+++ lib/pages/library_explore_page.dart
@@ -10,22 +10,23 @@ class LibraryExplorePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 1,
-      length: 3,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Library / Explore'),
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.cloud_outlined),
-              ),
-              Tab(
-                icon: Icon(Icons.beach_access_sharp),
-              ),
-              Tab(
-                icon: Icon(Icons.brightness_5_sharp),
-              ),
-            ],
+          title: Container(
+            width: 200,
+            child: const TabBar(
+              tabs: <Widget>[
+                Tab(
+                  text: 'Library',
+                  // icon: Icon(Icons.cloud_outlined),
+                ),
+                Tab(
+                  text: 'Explore',
+                  // icon: Icon(Icons.beach_access_sharp),
+                ),
+              ],
+            ),
           ),
         ),
         // body: Center(
@@ -36,16 +37,18 @@ class LibraryExplorePage extends StatelessWidget {
         //     child: const Text('Go back!'),
         //   ),
         // ),
-        body: const TabBarView(
+        body: TabBarView(
           children: <Widget>[
             Center(
-              child: Text("It's cloudy here"),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Go back!'),
+              ),
             ),
             Center(
               child: Text("It's rainy here"),
-            ),
-            Center(
-              child: Text("It's sunny here"),
             ),
           ],
         ),
