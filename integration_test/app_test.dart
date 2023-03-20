1d0e50e901c22ffa8522231fffb68c39dda26fb3
Khanin <khanin.dev@gmail.com>
2023-03-20 11:25:00 +0700

    clean up

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -13,35 +13,18 @@ void main() {
       app.main();
       await tester.pumpAndSettle();
 
-      // // Verify the counter starts at 0.
-      // expect(find.text('0'), findsOneWidget);
-
-      // // Finds the floating action button to tap on.
-      // final Finder fab = find.byTooltip('Increment');
-
-      // // Emulate a tap on the floating action button.
-      // await tester.tap(fab);
-
-      // // Trigger a frame.
-      // await tester.pumpAndSettle();
-
-      // // Verify the counter increments by 1.
-      // expect(find.text('1'), findsOneWidget);
-
       //open menu
       final colorRow = find.byKey(Key('COLOR_ROW_0'));
       await tester.tap(colorRow);
-
       await tester.pumpAndSettle();
 
+      //select add color
       final addButton = find.byIcon(Icons.add);
       await tester.tap(addButton);
       await tester.pumpAndSettle();
 
-      expect(find.byType(ColorRow), findsNWidgets(6));
-
-      //select add color
       //expect 1 more color
+      expect(find.byType(ColorRow), findsNWidgets(6));
     });
   });
 }
